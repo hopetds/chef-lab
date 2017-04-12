388a0da8fee06c030ad4a2dc15cf076009333588
Student <student@epbyminw1969.minsk.epam.com>
2017-04-12 04:08:50 +0300

    final nginx fix

---
+++ cookbooks/web_apache/metadata.rb
@@ -0,0 +1,7 @@
+name             'web_apache'
+maintainer       'ilya'
+maintainer_email 'ilya_khamiakou@epam.com'
+license          'All rights reserved'
+description      'Installs/Configures web_apache'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
